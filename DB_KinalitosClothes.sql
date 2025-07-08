8d2646f1207869b5e19167120d72c9e3c8f7358a
jjimenez-2021647 <jjimenez-2021647@kinal.edu.gt>
2025-07-08 14:35:07 -0600

    feat: Creacion de la base de datos y comentarios con nuestros datos

--- DB_KinalitosClothes.sql
+++ DB_KinalitosClothes.sql
@@ -1,1 +1,12 @@
-"Documento para la base de datos" 
+-- Por Josué Gilberto Jiménez Ajtún, Sebastian alejandro Molina Herrera, Rhandy Estuardo Cana Subuyj, Joshua Alejandro Santa Cruz Chicas, 
+-- Alejandro Jose Arocha Virula, Jefry Andre Cruz Yuman, Roberto Andre Rodriguez Gonzalez, Jeremy Jhoel Mendez Palencia
+-- 2021647, 2021528, 2021639, 2024254, 2024203, 2021644, 2023342, 2021550
+-- IN5BM
+-- 07/07/2025
+
+-- Creación de la base de datos
+Drop Database if exists DB_KinalitosClothes; 
+Create Database DB_KinalitosClothes;
+Use DB_KinalitosClothes;
+
+-- ---------- Entidades ----------
