c3a5e20064fba48f3c31b0fe404416d3cde1a5e7
jjimenez-2021647 <jjimenez-2021647@kinal.edu.gt>
2025-07-22 01:13:33 -0600

    feat: Se agrego el menu principal y se mando a llamr a la Principal - jjimenez-2021647

---
+++ KinalitosClothes/src/main/java/com/kinalitosclothes/system/Principal.java
@@ -0,0 +1,11 @@
+package com.josuejimenez.system;
+
+import com.josuejimenez.view.MenuPrincipal;
+
+public class Principal {
+
+    public static void main(String[] args) {
+        MenuPrincipal mp = new MenuPrincipal();
+        mp.mostrarMenu();
+    }
+}
