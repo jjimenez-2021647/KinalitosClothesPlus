a29e3817f588b710cb4d0c856bb78bec64a7b419
smolina-2021528 <smolina-2021528@kinal.edu.gt>
2025-07-23 11:50:38 -0600

    feat: Se agrego el mapeo con MVC implementado de la entidad Clientes

--- Java Mapeo de Datos/KinalitosClothes/src/main/java/com/kinalitosclothes/controller/ClientesController.java
+++ Java Mapeo de Datos/KinalitosClothes/src/main/java/com/kinalitosclothes/controller/ClientesController.java
@@ -1,164 +1,111 @@
 package com.kinalitosclothes.controller;
+
 import com.kinalitosclothes.dominio.Clientes;
-import com.kinalitosclothes.dominio.Usuarios;
-import java.util.List;
-import java.util.Scanner;
+import com.kinalitosclothes.view.MenuClientes;
+
 import javax.persistence.*;
+import java.util.List;
+
 public class ClientesController {
-    public void ClientesController() {
-        Scanner leer = new Scanner(System.in);
+    private MenuClientes view;
+    private EntityManager em;
+
+    public ClientesController() {
+        view = new MenuClientes();
         EntityManagerFactory emf = Persistence.createEntityManagerFactory("dominio");
-        EntityManager em = emf.createEntityManager();
-        int op = 1;
-        while (op != 0) {
-            System.out.println("-----------------------------------");
-            System.out.println("Bienvenido al menu de Clientes");
-            System.out.println("Por favor elija una opcion:");
-            System.out.println("1. Agregar");
-            System.out.println("2. Listar");
-            System.out.println("3. Eliminar");
-            System.out.println("4. Buscar");
-            System.out.println("5. Editar");
-            System.out.println("0. Salir");
-            System.out.println("-----------------------------------");
-            op = leer.nextInt();
-            switch (op) {
-                case 1:
-                    System.out.println("-----------------------------------");
-                    System.out.println("Agregar Cliente");
-                    leer.nextLine();
-                    System.out.println("Ingrese el nombre del cliente:");
-                    String nombre = leer.nextLine();
-                    System.out.println("Ingrese el apellido del cliente:");
-                    String apellido = leer.nextLine();
-                    System.out.println("Ingrese el correo del cliente:");
-                    String correo = leer.nextLine();
-                    System.out.println("Ingrese el telefono del cliente:");
-                    String telefono = leer.nextLine();
-                    System.out.println("Ingrese la direccion del cliente:");
-                    String direccion = leer.nextLine();
-                    System.out.println("Ingrese el codigo de usuario asociado:");
-                    int codUsuario = leer.nextInt();
-                    Usuarios usuario = em.find(Usuarios.class, codUsuario);
-                    if (usuario != null) {
-                        Clientes nuevoCliente = new Clientes();
-                        nuevoCliente.setNombreCliente(nombre);
-                        nuevoCliente.setApellidoCliente(apellido);
-                        nuevoCliente.setCorreoCliente(correo);
-                        nuevoCliente.setTelefonoCliente(telefono);
-                        nuevoCliente.setDireccionCliente(direccion);
-                        nuevoCliente.setUsuario(usuario);
-                        em.getTransaction().begin();
-                        em.persist(nuevoCliente);
-                        em.getTransaction().commit();
-                        System.out.println("Cliente agregado exitosamente.");
-                    } else {
-                        System.out.println("No se encontró un usuario con ese código.");
-                    }
-                    break;
-                case 2:
-                    System.out.println("-----------------------------------");
-                    System.out.println("Lista de todos los Clientes");
-                    TypedQuery<Clientes> query = em.createQuery("SELECT c FROM Cliente c", Clientes.class);
-                    List<Clientes> clientes = query.getResultList();
-                    for (Clientes c : clientes) {
-                        System.out.println("--------------------------------------");
-                        System.out.println(c);
-                    }
-                    break;
-                case 3:
-                    System.out.println("-----------------------------------");
-                    System.out.println("Eliminar Cliente");
-                    System.out.println("Ingrese el ID del cliente a eliminar");
-                    try {
-                        int codigo = leer.nextInt();
-                        leer.nextLine();
-                        Clientes clienteEliminar = em.find(Clientes.class, codigo);
-                        if (clienteEliminar != null) {
-                            System.out.println("¿Desea eliminar el siguiente cliente?");
-                            System.out.println(clienteEliminar);
-                            System.out.println("Escriba 'SI' para confirmar:");
-                            String confirmar = leer.nextLine();
-                            if (confirmar.equalsIgnoreCase("SI")) {
-                                em.getTransaction().begin();
-                                em.remove(clienteEliminar);
-                                em.getTransaction().commit();
-                                System.out.println("Cliente eliminado correctamente.");
-                            } else {
-                                System.out.println("Operación cancelada por el usuario.");
-                            }
-                        } else {
-                            System.out.println("No se encontró ningún cliente con ese ID.");
-                        }
-                    } catch (Exception e) {
-                        System.out.println("Error: Ingrese un ID válido.");
-                    }
-                    break;
-                case 4:
-                    System.out.println("-----------------------------------");
-                    System.out.println("Buscar Cliente");
-                    System.out.println("Ingrese el código del cliente:");
-                    int codigoBuscar = leer.nextInt();
-                    Clientes c = em.find(Clientes.class, codigoBuscar);
-                    System.out.println(c);
-                    break;
-                case 5:
-                    System.out.println("-----------------------------------");
-                    System.out.println("Editar Cliente");
-                    leer.nextLine();
-                    System.out.println("Ingrese el código del cliente a editar:");
-                    try {
-                        int codigoEditar = leer.nextInt();
-                        leer.nextLine();
-                        Clientes clienteEditar = em.find(Clientes.class, codigoEditar);
-                        if (clienteEditar != null) {
-                            System.out.println("Cliente encontrado:");
-                            System.out.println(clienteEditar);
-                            System.out.println("Ingrese el nuevo nombre:");
-                            String nuevoNombre = leer.nextLine();
-                            System.out.println("Ingrese el nuevo apellido:");
-                            String nuevoApellido = leer.nextLine();
-                            System.out.println("Ingrese el nuevo correo:");
-                            String nuevoCorreo = leer.nextLine();
-                            System.out.println("Ingrese el nuevo telefono:");
-                            String nuevoTelefono = leer.nextLine();
-                            System.out.println("Ingrese la nueva direccion:");
-                            String nuevaDireccion = leer.nextLine();
-                            if (!nuevoNombre.isEmpty()) {
-                                clienteEditar.setNombreCliente(nuevoNombre);
-                            }
-                            if (!nuevoApellido.isEmpty()) {
-                                clienteEditar.setApellidoCliente(nuevoApellido);
-                            }
-                            if (!nuevoCorreo.isEmpty()) {
-                                clienteEditar.setCorreoCliente(nuevoCorreo);
-                            }
-                            if (!nuevoTelefono.isEmpty()) {
-                                clienteEditar.setTelefonoCliente(nuevoTelefono);
-                            }
-                            if (!nuevaDireccion.isEmpty()) {
-                                clienteEditar.setDireccionCliente(nuevaDireccion);
-                            }
-                            em.getTransaction().begin();
-                            em.merge(clienteEditar);
-                            em.getTransaction().commit();
-                            System.out.println("Cliente actualizado exitosamente.");
-                        } else {
-                            System.out.println("No se encontró ningún cliente con ese ID.");
-                        }
-                    } catch (Exception e) {
-                        System.out.println("Error: Ingrese un ID válido.");
-                    }
-                    break;
-                case 0:
-                    System.out.println("-----------------------------------");
-                    System.out.println("Saliendo...");
-                    emf.close();
-                    em.close();
-                    break;
-                default:
-                    System.out.println("Por favor seleccione una de las opciones válidas.");
+        em = emf.createEntityManager();
+    }
+
+    public void iniciar() {
+        int op;
+        do {
+            op = view.mostrarMenuYObtenerOpcion();
+            procesarOpcion(op);
+        } while (op != 0);
+        em.close();
+    }
+
+    private void procesarOpcion(int op) {
+        switch (op) {
+            case 1:
+                agregarCliente();
+                break;
+            case 2:
+                listarClientes();
+                break;
+            case 3:
+                eliminarCliente();
+                break;
+            case 4:
+                buscarCliente();
+                break;
+            case 5:
+                editarCliente();
+                break;
+            case 0:
+                view.mostrarMensaje("Saliendo...");
+                break;
+            default:
+                view.mostrarMensaje("Por favor seleccione una de las opciones válidas");
+        }
+    }
+
+    private void agregarCliente() {
+        Clientes nuevoCliente = view.obtenerDatosCliente();
+        em.getTransaction().begin();
+        em.persist(nuevoCliente);
+        em.getTransaction().commit();
+        view.mostrarMensaje("Cliente agregado exitosamente.");
+    }
+
+    private void listarClientes() {
+        TypedQuery<Clientes> query = em.createQuery("SELECT c FROM Clientes c", Clientes.class);
+        List<Clientes> clientes = query.getResultList();
+        view.mostrarClientes(clientes);
+    }
+
+    private void eliminarCliente() {
+        int codigoCliente = view.obtenerIdCliente();
+        Clientes clienteAEliminar = em.find(Clientes.class, codigoCliente);
+        if (clienteAEliminar != null) {
+            view.mostrarClientes(List.of(clienteAEliminar));
+            if (view.confirmarEliminacion().equalsIgnoreCase("SI")) {
+                em.getTransaction().begin();
+                StoredProcedureQuery spq = em.createNamedStoredProcedureQuery("Cliente.eliminar");
+                spq.setParameter("codigoCliente", codigoCliente);
+                spq.execute();
+                em.getTransaction().commit();
+                view.mostrarMensaje("Cliente eliminado correctamente.");
+            } else {
+                view.mostrarMensaje("Operación cancelada por el usuario.");
             }
+        } else {
+            view.mostrarMensaje("No se encontró ningún cliente con ese ID.");
+        }
+    }
+
+    private void buscarCliente() {
+        int codigoCliente = view.obtenerIdCliente();
+        Clientes cliente = em.find(Clientes.class, codigoCliente);
+        if (cliente != null) {
+            view.mostrarClientes(List.of(cliente));
+        } else {
+            view.mostrarMensaje("No se encontró ningún cliente con ese ID.");
+        }
+    }
+
+    private void editarCliente() {
+        int codigoClienteEditar = view.obtenerIdCliente();
+        Clientes clienteAEditar = em.find(Clientes.class, codigoClienteEditar);
+        if (clienteAEditar != null) {
+            view.mostrarClientes(List.of(clienteAEditar));
+            Clientes clienteActualizado = view.obtenerDatosActualizados(clienteAEditar);
+            em.getTransaction().begin();
+            em.merge(clienteActualizado);
+            em.getTransaction().commit();
+            view.mostrarMensaje("Cliente actualizado exitosamente.");
+        } else {
+            view.mostrarMensaje("No se encontró ningún cliente con ese ID.");
         }
     }
 }
--- Java Mapeo de Datos/KinalitosClothes/src/main/java/com/kinalitosclothes/view/MenuClientes.java
+++ Java Mapeo de Datos/KinalitosClothes/src/main/java/com/kinalitosclothes/view/MenuClientes.java
@@ -1,13 +1,97 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package com.kinalitosclothes.view;
 
-/**
- *
- * @author informatica
- */
+import com.kinalitosclothes.dominio.Clientes;
+
+import java.util.List;
+import java.util.Scanner;
+
 public class MenuClientes {
-    
+    private Scanner scanner;
+
+    public MenuClientes() {
+        scanner = new Scanner(System.in);
+    }
+
+    public int mostrarMenuYObtenerOpcion() {
+        System.out.println("-----------------------------------");
+        System.out.println("Bienvenido al menú de Clientes");
+        System.out.println("Por favor elija una opción:");
+        System.out.println("1. Agregar");
+        System.out.println("2. Listar");
+        System.out.println("3. Eliminar");
+        System.out.println("4. Buscar");
+        System.out.println("5. Editar");
+        System.out.println("0. Salir");
+        System.out.println("-----------------------------------");
+        return scanner.nextInt();
+    }
+
+    public Clientes obtenerDatosCliente() {
+        System.out.println("Agregar Cliente");
+        System.out.println("Ingrese el código del cliente:");
+        int codigoCliente = scanner.nextInt();
+        scanner.nextLine(); // Limpiar buffer
+        System.out.println("Ingrese el nombre del cliente:");
+        String nombre = scanner.nextLine();
+        System.out.println("Ingrese el apellido del cliente:");
+        String apellido = scanner.nextLine();
+        System.out.println("Ingrese el correo del cliente:");
+        String correo = scanner.nextLine();
+        System.out.println("Ingrese el teléfono del cliente:");
+        String telefono = scanner.nextLine();
+        System.out.println("Ingrese la dirección del cliente:");
+        String direccion = scanner.nextLine();
+        System.out.println("Ingrese el código del usuario asociado:");
+        int codigoUsuario = scanner.nextInt();
+
+        return new Clientes(codigoCliente, nombre, apellido, correo, telefono, direccion, codigoUsuario);
+    }
+
+    public void mostrarClientes(List<Clientes> clientes) {
+        System.out.println("Lista de todos los Clientes");
+        for (Clientes cliente : clientes) {
+            System.out.println("--------------------------------------");
+            System.out.println(cliente);
+        }
+    }
+
+    public int obtenerIdCliente() {
+        System.out.println("Ingrese el ID del cliente");
+        return scanner.nextInt();
+    }
+
+    public String confirmarEliminacion() {
+        System.out.println("¿El cliente que desea eliminar es el siguiente?");
+        System.out.println("Escriba 'SI' para confirmar:");
+        return scanner.next();
+    }
+
+    public void mostrarMensaje(String mensaje) {
+        System.out.println(mensaje);
+    }
+
+    public Clientes obtenerDatosActualizados(Clientes cliente) {
+        scanner.nextLine(); // Consumir salto de línea pendiente
+        System.out.println("Ingrese el nuevo nombre del cliente:");
+        String nuevoNombre = scanner.nextLine();
+        System.out.println("Ingrese el nuevo apellido del cliente:");
+        String nuevoApellido = scanner.nextLine();
+        System.out.println("Ingrese el nuevo correo del cliente:");
+        String nuevoCorreo = scanner.nextLine();
+        System.out.println("Ingrese el nuevo teléfono del cliente:");
+        String nuevoTelefono = scanner.nextLine();
+        System.out.println("Ingrese la nueva dirección del cliente:");
+        String nuevaDireccion = scanner.nextLine();
+        System.out.println("Ingrese el nuevo código de usuario:");
+        int nuevoCodigoUsuario = scanner.nextInt();
+
+        cliente.setNombreCliente(nuevoNombre);
+        cliente.setApellidoCliente(nuevoApellido);
+        cliente.setCorreoCliente(nuevoCorreo);
+        cliente.setTelefonoCliente(nuevoTelefono);
+        cliente.setDireccionCliente(nuevaDireccion);
+        cliente.setCodigoUsuario(nuevoCodigoUsuario);
+
+        return cliente;
+    }
 }
