edbc9994d11931df67753dc0b3ea0eb64b0a1baa
rrodriguez-2023342 <rrodriguez-2023342@kinal.edu.gt>
2025-07-23 08:38:41 -0600

    feat: Se agrego el mapeo de la entidad Pedidos

---
+++ Java Mapeo de Datos/KinalitosClothes/target/generated-sources/annotations/com/kinalitosclothes/dominio/Clientes_.java
@@ -0,0 +1,20 @@
+package com.kinalitosclothes.dominio;
+
+import com.kinalitosclothes.dominio.Usuarios;
+import javax.annotation.processing.Generated;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@Generated(value="org.eclipse.persistence.internal.jpa.modelgen.CanonicalModelProcessor", date="2025-07-23T08:36:49", comments="EclipseLink-2.7.12.v20230209-rNA")
+@StaticMetamodel(Clientes.class)
+public class Clientes_ { 
+
+    public static volatile SingularAttribute<Clientes, String> correoCliente;
+    public static volatile SingularAttribute<Clientes, String> nombreCliente;
+    public static volatile SingularAttribute<Clientes, Integer> codigoCliente;
+    public static volatile SingularAttribute<Clientes, String> apellidoCliente;
+    public static volatile SingularAttribute<Clientes, String> telefonoCliente;
+    public static volatile SingularAttribute<Clientes, String> direccionCliente;
+    public static volatile SingularAttribute<Clientes, Usuarios> usuario;
+
+}
