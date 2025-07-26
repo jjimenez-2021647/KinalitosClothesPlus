522c95de9f1ec86ecca426d39f7c8160c34c9866
jjimenez-2021647 <jjimenez-2021647@kinal.edu.gt>
2025-07-26 13:32:43 -0600

    fix: Se corrigio el formato de un archivo

--- Java Mapeo de Datos/KinalitosClothes/target/generated-sources/annotations/com/kinalitosclothes/dominio/DetallePedidos_.java
+++ Java Mapeo de Datos/KinalitosClothes/target/generated-sources/annotations/com/kinalitosclothes/dominio/DetallePedidos_.java
@@ -4,7 +4,7 @@ import javax.annotation.processing.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="org.eclipse.persistence.internal.jpa.modelgen.CanonicalModelProcessor", date="2025-07-23T13:12:00", comments="EclipseLink-2.7.12.v20230209-rNA")
+@Generated(value="org.eclipse.persistence.internal.jpa.modelgen.CanonicalModelProcessor", date="2025-07-26T13:17:49", comments="EclipseLink-2.7.12.v20230209-rNA")
 @StaticMetamodel(DetallePedidos.class)
 public class DetallePedidos_ { 
 
